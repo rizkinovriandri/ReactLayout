745bcbaa2d95be257becf3d83bd3e2908157f7c9
rizkinovriandri <qieroway@gmail.com>
2020-08-30 12:53:00 +0700

    Restore Button

--- src/components/BtnJoin/BtnJoin.js
+++ src/components/BtnJoin/BtnJoin.js
@@ -1,24 +1,10 @@
 import React from 'react';
-// import React, { Component } from 'react';
-import ButtonToolbar from '@bit/react-bootstrap.react-bootstrap.button-toolbar';
-import Button from '@bit/react-bootstrap.react-bootstrap.button';
-// import ReactBootstrapStyle from '@bit/react-bootstrap.react-bootstrap.internal.style-links';
 
 import './BtnJoin.css';
 
 const BtnJoin = (props) => {
     return (
-        <>
-        <button variant="primary">{props.text}</button>
-        <ButtonToolbar>
-				<Button variant="primary" size="lg" active>
-					Primary button
-          </Button>
-				<Button variant="secondary" size="lg" active style={{ marginLeft: 5 }}>
-					Button
-          </Button>
-			</ButtonToolbar>
-        </>
+        <button>{props.text}</button>
     );
 }
 
